22c93dda017c5a95d355e97253034a6900177531
AvigdorFeldman <avigdor.feldman@e.braude.ac.il>
2025-08-02 21:39:05 +0300

    Active sessions fix

--- Client/src/clientControllers/ViewActiveSessionsController.java
+++ Client/src/clientControllers/ViewActiveSessionsController.java
@@ -1,10 +1,5 @@
 package clientControllers;
 
-import java.awt.Desktop;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
